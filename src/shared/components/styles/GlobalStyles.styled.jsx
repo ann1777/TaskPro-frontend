9e07fb287ed43e3d6655d0643d4b76564a6ea527
Slavska <slavskaya.anastasiia.o@gmail.com>
2023-08-24 00:24:56 +0300

    global styles

---
+++ src/shared/components/styles/GlobalStyles.styled.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+body {
+  color: black;
+  font-family: 'Poppins', -apple-system, sans-serif;
+  font-weight: 500;
+  font-size: 14px;
+  letter-spacing: 0.04em;
+  line-height: 1.21;
+  width: 100vw;
+  min-height: 100vh;
+  background: white;
+  }`;
