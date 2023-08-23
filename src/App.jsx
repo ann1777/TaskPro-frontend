8a299544e5c23fc5f8131520e765a56ec475bff7
Slavska <slavskaya.anastasiia.o@gmail.com>
2023-08-24 00:24:31 +0300

    Add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Login from "./shared/components/Login/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Registration from "./shared/components/Registration/Registration";
+import Welcome from "./shared/components/Welcome/Welcome";
+import { GlobalStyles } from "./shared/components/styles/GlobalStyles.styled";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+      </style>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route index element={<Welcome />} />
+          <Route path="/login" element={<Login />} restricted />
+          <Route path="/registration" element={<Registration />} restricted />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
