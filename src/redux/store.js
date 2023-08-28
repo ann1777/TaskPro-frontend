f4e1b6577d3dabb2a89325e9fd63b4f0311b1d41
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-08-28 16:03:30 +0300

    Configure store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,47 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { authReducer } from './auth/authSlice';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist/es/constants';
+import { authSlice } from './auth/authSlice';
 import { dashboardReducer } from './dashboards/dashboardsSlice';
+import { themeSlice } from './theme/theme-reducer';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+// Persisting token field from auth slice to local storage
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['accessToken', 'refreshToken', 'theme'],
+};
+
+// Persisting theme slice to local storage
+const themePersistConfig = {
+  key: 'theme',
+  storage,
+};
+
+const rootReducer = {
+  auth: persistReducer(authPersistConfig, authSlice.reducer),
+  theme: persistReducer(themePersistConfig, themeSlice.reducer),
+  dashboards: dashboardReducer,
+};
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    dashboards: dashboardReducer,
-  },
+  reducer: rootReducer,
+  devTools: isDevelopment,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
+
+export const persistor = persistStore(store);
