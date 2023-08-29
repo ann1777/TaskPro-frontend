c4d376ebc647b81950f4f730d37ae01ad0104e99
Oksana Yerep <114570807+Oksana-y@users.noreply.github.com>
2023-08-29 09:44:02 +0300

    Changes in HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,28 @@
 import { Header } from "../../shared/components/Header/Header";
 import { Sidebar } from "../../shared/components/Sidebar/Sidebar";
 import { GlobalStylesHome } from "../../shared/components/styles/GlobalStyles.styled";
+import { Modal } from "../../shared/components/Modal/Modal";
+import AddBoard from "../../shared/components/Modal/AddBoard/AddBoard";
+import { useState } from "react";
+
+
 
 export const HomePage = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(true);
+  };
+
   return (
     <>
       <GlobalStylesHome />
-      <Sidebar />
+      <Sidebar onOpen={toggleModal}/>
+      {modalOpen && (
+        <Modal>
+          <AddBoard />
+        </Modal>
+      )}
       <Header />
     </>
   );
