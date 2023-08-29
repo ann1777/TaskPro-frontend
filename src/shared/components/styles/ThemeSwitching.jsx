1d0c59019d3327c462474f1a113373406896b785
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-08-29 12:15:14 +0300

    Fixed import theme

--- src/shared/components/styles/ThemeSwitching.jsx
+++ src/shared/components/styles/ThemeSwitching.jsx
@@ -1,17 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { useSelector } from "react-redux";
-import PropTypes from "prop-types";
-import { selectUserTheme } from "../../../redux/auth/authSelectors";
-import { light, dark, color } from "./Theme.styled";
+import { ThemeProvider } from 'styled-components';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import { selectUserTheme } from '../../../redux/auth/authSelectors';
+import { light, dark, color } from './theme.styled';
 
 export const ThemeSwitching = ({ children }) => {
   const activeUserTheme = useSelector(selectUserTheme);
   const HandleThemeChoose = () => {
-    if (activeUserTheme === "light") {
+    if (activeUserTheme === 'light') {
       return light;
-    } else if (activeUserTheme === "dark") {
+    } else if (activeUserTheme === 'dark') {
       return dark;
-    } else if (activeUserTheme === "color") {
+    } else if (activeUserTheme === 'color') {
       return color;
     }
     return light;
