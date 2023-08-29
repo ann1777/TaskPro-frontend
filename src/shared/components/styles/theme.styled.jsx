cf572159016f0492e7934cf5ec4398d99a1830c3
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-08-29 12:24:05 +0300

    Fixed theme export

--- src/shared/components/styles/theme.styled.jsx
+++ src/shared/components/styles/theme.styled.jsx
@@ -1,134 +1,135 @@
 export const light = {
-  name: "light",
+  name: 'light',
   colors: {
     //   SIDEBAR
 
-    secondTextSidebar: "rgba(22, 22, 22, 0.50)",
-    textTitlesSidebar: "#161616",
-    plusBtnColorSidebar: "#121212",
-    plusBtnBackgroundSidebar: "#BEDBB0",
-    backgroundActiveSidebar: "#F6F6F7",
-    backgroundWindowSidebar: "#F6F6F7",
-    backroundSidebar: "#FFF",
-    logoBackground: "#1F1F1F",
-    logoColor: "#FFFFFF",
-    btnLogOut: "#BEDBB0",
-    btnLogOutActive: "#9DC888",
+    secondTextSidebar: 'rgba(22, 22, 22, 0.50)',
+    textTitlesSidebar: '#161616',
+    plusBtnColorSidebar: '#121212',
+    plusBtnBackgroundSidebar: '#BEDBB0',
+    backgroundActiveSidebar: '#F6F6F7',
+    backgroundWindowSidebar: '#F6F6F7',
+    backroundSidebar: '#FFF',
+    logoBackground: '#1F1F1F',
+    logoColor: '#FFFFFF',
+    btnLogOut: '#BEDBB0',
+    btnLogOutActive: '#9DC888',
 
     // HEADER
 
-    textHeader: "rgba(22, 22, 22, 0.80)",
-    textHeaderActive: "#161616",
-    backgroundHeader: "#FCFCFC",
+    textHeader: 'rgba(22, 22, 22, 0.80)',
+    textHeaderActive: '#161616',
+    backgroundHeader: '#FCFCFC',
 
     // DASHBOARD
 
-    textTitleMain: "#161616",
-    iconAndSubTextMain: "rgba(22, 22, 22, 0.50)",
-    textCardMain: "rgba(22, 22, 22, 0.70)",
-    backgroundMain: "#F6F6F7",
-    backgroundCard: "#FFF",
-    textFilters: "rgba(22, 22, 22, 0.80)",
-    scrollLineColor: "#E8E8E8",
-    scrollColor: "rgba(22, 22, 22, 0.10)",
-    btnAccentBackground: "#BEDBB0",
-    btnAccentText: "#161616",
+    textTitleMain: '#161616',
+    iconAndSubTextMain: 'rgba(22, 22, 22, 0.50)',
+    textCardMain: 'rgba(22, 22, 22, 0.70)',
+    backgroundMain: '#F6F6F7',
+    backgroundCard: '#FFF',
+    textFilters: 'rgba(22, 22, 22, 0.80)',
+    scrollLineColor: '#E8E8E8',
+    scrollColor: 'rgba(22, 22, 22, 0.10)',
+    btnAccentBackground: '#BEDBB0',
+    btnAccentText: '#161616',
 
     // MODAL
 
-    backgroundColorModal: "#FCFCFC",
-    textColorModal: "#161616",
-    textSecondModal: "rgba(22, 22, 22, 0.50)",
-    borderColorModal: "1px solid #BEDBB0",
+    backgroundColorModal: '#FCFCFC',
+    textColorModal: '#161616',
+    textSecondModal: 'rgba(22, 22, 22, 0.50)',
+    borderColorModal: '1px solid #BEDBB0',
   },
 };
 export const dark = {
-  name: "dark",
+  name: 'dark',
   colors: {
     //   SIDEBAR
 
-    secondTextSidebar: "rgba(255, 255, 255, 0.50)",
-    textTitlesSidebar: "#FFF",
-    plusBtnColorSidebar: "#121212",
-    plusBtnBackgroundSidebar: "#BEDBB0",
-    backgroundActiveSidebar: "#1F1F1F",
-    backgroundWindowSidebar: "#1F1F1F",
-    backroundSidebar: "#121212",
-    logoBackground: "#1F1F1F",
-    logoColor: "#FFFFFF",
-    btnLogOut: "#BEDBB0",
-    btnLogOutActive: "#9DC888",
+    secondTextSidebar: 'rgba(255, 255, 255, 0.50)',
+    textTitlesSidebar: '#FFF',
+    plusBtnColorSidebar: '#121212',
+    plusBtnBackgroundSidebar: '#BEDBB0',
+    backgroundActiveSidebar: '#1F1F1F',
+    backgroundWindowSidebar: '#1F1F1F',
+    backroundSidebar: '#121212',
+    logoBackground: '#1F1F1F',
+    logoColor: '#FFFFFF',
+    btnLogOut: '#BEDBB0',
+    btnLogOutActive: '#9DC888',
 
     // HEADER
 
-    textHeader: "rgba(22, 22, 22, 0.80)",
-    textHeaderActive: "#161616",
-    backgroundHeader: "#FCFCFC",
+    textHeader: 'rgba(22, 22, 22, 0.80)',
+    textHeaderActive: '#161616',
+    backgroundHeader: '#FCFCFC',
 
     // DASHBOARD
 
-    textTitleMain: "#fff",
-    iconAndSubTextMain: "rgba(255, 255, 255, 0.50)",
-    textCardMain: "rgba(255, 255, 255, 0.50)",
-    backgroundMain: "#ECEDFD",
-    backgroundCard: "#121212",
-    textFilters: "rgba(255, 255, 255, 0.80)",
-    scrollLineColor: "#FFF",
-    scrollColor: "#121212",
-    btnAccentBackground: "#BEDBB0",
-    btnAccentText: "#161616",
+    textTitleMain: '#fff',
+    iconAndSubTextMain: 'rgba(255, 255, 255, 0.50)',
+    textCardMain: 'rgba(255, 255, 255, 0.50)',
+    backgroundMain: '#ECEDFD',
+    backgroundCard: '#121212',
+    textFilters: 'rgba(255, 255, 255, 0.80)',
+    scrollLineColor: '#FFF',
+    scrollColor: '#121212',
+    btnAccentBackground: '#BEDBB0',
+    btnAccentText: '#161616',
 
     // MODAL
 
-    backgroundColorModal: "#151515",
-    textColorModal: "#FFF",
-    textSecondModal: "#FFF",
-    borderColorModal: "1px solid #BEDBB0",
+    backgroundColorModal: '#151515',
+    textColorModal: '#FFF',
+    textSecondModal: '#FFF',
+    borderColorModal: '1px solid #BEDBB0',
   },
 };
 export const color = {
-  name: "color",
+  name: 'color',
   colors: {
     //   SIDEBAR
 
-    secondTextSidebar: "rgba(255, 255, 255, 0.50)",
-    textTitlesSidebar: "#FFF",
-    plusBtnColorSidebar: "#FFF",
-    plusBtnBackgroundSidebar: "#B8BCFD",
-    backgroundActiveSidebar: "rgba(255, 255, 255, 0.50)",
-    backgroundWindowSidebar: "rgba(236, 237, 253, 0.40)",
-    backroundSidebar: "#5255BC",
-    logoBackground: "#ECEDFD",
-    logoColor: "##5255BC",
-    btnLogOut: "#FFF",
-    btnLogOutActive: "#B8BCFD",
+    secondTextSidebar: 'rgba(255, 255, 255, 0.50)',
+    textTitlesSidebar: '#FFF',
+    plusBtnColorSidebar: '#FFF',
+    plusBtnBackgroundSidebar: '#B8BCFD',
+    backgroundActiveSidebar: 'rgba(255, 255, 255, 0.50)',
+    backgroundWindowSidebar: 'rgba(236, 237, 253, 0.40)',
+    backroundSidebar: '#5255BC',
+    logoBackground: '#ECEDFD',
+    logoColor: '##5255BC',
+    btnLogOut: '#FFF',
+    btnLogOutActive: '#B8BCFD',
 
     // HEADER
 
-    textHeader: "rgba(255, 255, 255, 0.80)",
-    textHeaderActive: "#FFF",
-    backgroundHeader: "#161616",
+    textHeader: 'rgba(255, 255, 255, 0.80)',
+    textHeaderActive: '#FFF',
+    backgroundHeader: '#161616',
 
     // DASHBOARD
 
-    textTitleMain: "#161616",
-    iconAndSubTextMain: "rgba(22, 22, 22, 0.50)",
-    textCardMain: "rgba(22, 22, 22, 0.70)",
-    backgroundMain: "#1F1F1F",
-    backgroundCard: "#FFF",
-    textFilters: "rgba(22, 22, 22, 0.80)",
-    scrollLineColor: "#FFF",
-    scrollColor: "#B8BCFD",
-    btnAccentBackground: "#5255BC",
-    btnAccentText: "#FFF",
+    textTitleMain: '#161616',
+    iconAndSubTextMain: 'rgba(22, 22, 22, 0.50)',
+    textCardMain: 'rgba(22, 22, 22, 0.70)',
+    backgroundMain: '#1F1F1F',
+    backgroundCard: '#FFF',
+    textFilters: 'rgba(22, 22, 22, 0.80)',
+    scrollLineColor: '#FFF',
+    scrollColor: '#B8BCFD',
+    btnAccentBackground: '#5255BC',
+    btnAccentText: '#FFF',
 
     // MODAL
 
-    backgroundColorModal: "#FCFCFC",
-    textColorModal: "#161616",
-    textSecondModal: "rgba(22, 22, 22, 0.50)",
-    borderColorModal: "1px solid #5255BC",
+    backgroundColorModal: '#FCFCFC',
+    textColorModal: '#161616',
+    textSecondModal: 'rgba(22, 22, 22, 0.50)',
+    borderColorModal: '1px solid #5255BC',
   },
 };
-];
+
+const theme = { light, dark, color };
 export default theme;
