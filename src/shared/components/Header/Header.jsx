275378d37a867758be2b8b0c163e0c98b88bdd06
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-08-30 16:58:19 +0300

    Added styles and components

--- src/shared/components/Header/Header.jsx
+++ src/shared/components/Header/Header.jsx
@@ -1,77 +1,73 @@
-import { useState } from "react";
-import sprite from "../../images/header-burger.svg";
+// import { useState } from "react";
+// import sprite from "../../images/header-burger.svg";
 import {
   StyledHeader,
   Wrapper,
   AvatarImg,
-  StyledSelect,
+  // StyledSelect,
   UserName,
   StyledSvgBurger,
   ButtonBurger,
-} from "./Header.styled";
-import { useDispatch } from "react-redux";
-import { useToggle } from "../../hooks/useToggle.js";
-import { changeTheme } from "../../../redux/auth/operations";
-import { selectUserTheme } from "../../../redux/auth/authSelectors";
-import { useSelector } from "react-redux";
-import { selectUser } from "../../../redux/auth/authSelectors";
-import { Modal } from "../Modal/Modal";
-import { EditProfile } from "../Modal/EditProfile/EditProfile";
-import { ThemeSwitcher } from "../Theme/ThemeSwitcher";
+} from './Header.styled';
+// import { useDispatch } from "react-redux";
+// import { useToggle } from "../../hooks/useToggle.js";
+// import { changeTheme } from "../../../redux/auth/operations";
+// import { selectUserTheme } from "../../../redux/auth/authSelectors";
+// import { useSelector } from "react-redux";
+// import { selectUser } from "../../../redux/auth/authSelectors";
+// import { Modal } from "../Modal/Modal";
+// import { EditProfile } from "../Modal/EditProfile/EditProfile";
+// import { ThemeSwitcher } from "../Theme/ThemeSwitcher";
 
-const options = [
-  { value: "light", label: "Light" },
-  { value: "color", label: "Dark" },
-  { value: "dark", label: "Violet" },
-];
+// const options = [
+//   { value: "light", label: "Light" },
+//   { value: "color", label: "Dark" },
+//   { value: "dark", label: "Violet" },
+// ];
 
-export const Header = ({ onOpenSidebar, openSidebar }) => {
-  const { name, avatarURL } = useSelector(selectUser);
-  // const [isOpenMenu, setIsOpenMenu] = useState(false);
-  const { isOpen, open } = useToggle();
+// export const Header = ({ onOpenSidebar, openSidebar }) => {
+//   const { name, avatarURL } = useSelector(selectUser);
+//   // const [isOpenMenu, setIsOpenMenu] = useState(false);
+//   const { isOpen, open } = useToggle();
 
-  //   const [isUserLogin, setIsUserLogin]=useState(false);
-  // const [isSelectedTheme,setIsSelectedTheme]=useState(false);
-  const dispatch = useDispatch();
+//   //   const [isUserLogin, setIsUserLogin]=useState(false);
+//   // const [isSelectedTheme,setIsSelectedTheme]=useState(false);
+//   const dispatch = useDispatch();
 
-  const handleThemeChange = (selectedOption) => {
-    dispatch(changeTheme(selectedOption.value));
-  };
-  
-  const closeModal = () => {
-        setModalOpen(false);
-    };
-  const [selectedOption] = useState(null);
-  const customStyles = {
-    option: (defaultStyles) => ({
-      ...defaultStyles,
-      // color: state.isSelected ? "#212529" : "#fff",
-      // borderRadius: "8px",
-      // border: "1px solid #BEDBB0",
+//   const handleThemeChange = (selectedOption) => {
+//     dispatch(changeTheme(selectedOption.value));
+//   };
 
-      // boxShadow: "0px 4px 16px 0px rgba(17, 17, 17, 0.10)",
+//   const closeModal = () => {
+//         setIsOpenOpen(false);
+//     };
+//   const [selectedOption] = useState(null);
+//   const customStyles = {
+//     option: (defaultStyles) => ({
+//       ...defaultStyles,
+//       // color: state.isSelected ? "#212529" : "#fff",
+//       // borderRadius: "8px",
+//       // border: "1px solid #BEDBB0",
 
+//       // boxShadow: "0px 4px 16px 0px rgba(17, 17, 17, 0.10)",
 
-  // const [selectedOption, setSelectedOption] = useState("");
-
+//       // const [selectedOption, setSelectedOption] = useState("");
+//     }),
+//   };
+export const Header = () => {
   return (
     <>
       <StyledHeader>
-        <ButtonBurger onClick={openSidebar}>
-          <StyledSvgBurger>
-            <use href={sprite + "#icon-burger"}></use>
-          </StyledSvgBurger>
-        </ButtonBurger>
-        <Wrapper>
+        {/* <Wrapper>
           <ThemeSwitcher />
           <UserName>{name}</UserName>
-          <AvatarImg onClick={open} src={avatarURL} alt="user" />
+          <AvatarImg onClick={open} src={avatarURL} alt='user' />
           {isOpen && (
             <Modal onClose={closeModal}>
               <EditProfile />
             </Modal>
           )}
-        </Wrapper>
+        </Wrapper> */}
       </StyledHeader>
     </>
   );
