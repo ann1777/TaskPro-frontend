72b21e17b28299c1aec9854350bf10529482a820
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-08-30 18:10:57 +0300

    Fixed spell mistake in HomePage styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,59 +1,77 @@
-import { useState } from "react";
-import { useToggle } from "../../shared/hooks/useToggle";
+import { useState } from 'react';
+import { useToggle } from '../../shared/hooks/useToggle';
 
-import NeedHelp from "../../shared/components/Modal/NeedHelp/NeedHelp";
-import { Header } from "../../shared/components/Header/Header";
-import { Sidebar } from "../../shared/components/Sidebar/Sidebar";
-import { Modal } from "../../shared/components/Modal/Modal";
-import AddBoard from "../../shared/components/Modal/AddBoard/AddBoard";
-import Dashboard from "../../shared/components/Dashboard/Dashboard";
+import NeedHelp from '../../shared/components/Modal/NeedHelp/NeedHelp';
+import { Header } from '../../shared/components/Header/Header';
+import { Sidebar } from '../../shared/components/Sidebar/Sidebar';
+import { Modal } from '../../shared/components/Modal/Modal';
+import AddBoard from '../../shared/components/Modal/AddBoard/AddBoard';
+import Dashboard from '../../shared/components/Dashboard/Dashboard';
 
-import { GlobalStylesHome } from "../../shared/components/styles/GlobalStyles.styled";
-import AddColumn from "../../shared/components/Modal/AddColumn/AddColumn";
+import { GlobalStylesHome } from '../../shared/components/styles/GlobalStyles.styled';
+import * as css from './HomePage.styled';
 
 export const HomePage = () => {
-    const [modalOpen, setModalOpen] = useState(false);
-    const [helpModalOpen, setHelpModalOpen] = useState(false);
-    const { isOpen, toggle } = useToggle();
+  const [modalOpen, setModalOpen] = useState(false);
+  const [helpModalOpen, setHelpModalOpen] = useState(false);
+  const { isOpen, toggle } = useToggle();
 
-    const toggleModal = () => {
-        setModalOpen(true);
-    };
+  const toggleModal = () => {
+    setModalOpen(true);
+  };
 
-    const openHelpModal = () => {
-        setHelpModalOpen(true);
-    };
+  const openHelpModal = () => {
+    setHelpModalOpen(true);
+  };
 
-    const closeHelpModal = () => {
-        setHelpModalOpen(false);
-    };
+  const closeHelpModal = () => {
+    setHelpModalOpen(false);
+  };
 
-    const closeModal = () => {
-        setModalOpen(false);
-    };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
-    return (
-        <>
-            <GlobalStylesHome />
-            <Sidebar
-                closeSidebar={toggle}
-                isOpen={isOpen}
-                onOpen={toggleModal}
-                onOpenHelp={openHelpModal}
-            />
+  return (
+    <>
+      <GlobalStylesHome />
+      <css.FlexDiv>
+        <Sidebar
+          closeSidebar={toggle}
+          isOpen={isOpen}
+          onOpen={toggleModal}
+          onOpenHelp={openHelpModal}
+        />
 
-            {helpModalOpen && (
-                <Modal onClose={closeHelpModal}>
-                    <NeedHelp onClose={closeHelpModal} />
-                </Modal>
-            )}
-            {modalOpen && (
-                <Modal onClose={closeModal}>
-                    <AddBoard onClose={closeModal} />
-                </Modal>
-            )}
-            <Header openSidebar={toggle} />
-            <Dashboard />
-        </>
-    );
+        {helpModalOpen && (
+          <Modal>
+            <NeedHelp />
+          </Modal>
+        )}
+        <css.HeadBoardDiv>
+          <Header openSidebar={toggle} />
+          <Dashboard />
+        </css.HeadBoardDiv>
+      </css.FlexDiv>
+      <Sidebar
+        closeSidebar={toggle}
+        isOpen={isOpen}
+        onOpen={toggleModal}
+        onOpenHelp={openHelpModal}
+      />
+
+      {helpModalOpen && (
+        <Modal onClose={closeHelpModal}>
+          <NeedHelp onClose={closeHelpModal} />
+        </Modal>
+      )}
+      {modalOpen && (
+        <Modal onClose={closeModal}>
+          <AddBoard onClose={closeModal} />
+        </Modal>
+      )}
+      <Header openSidebar={toggle} />
+      <Dashboard />
+    </>
+  );
 };
---
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const FlexDiv = styled.div`
+  display: flex;
+`;
+export const HeadBoardDiv = styled.div`
+  width: 100%;
+`;
