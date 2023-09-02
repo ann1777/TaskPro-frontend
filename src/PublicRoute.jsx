20f11754be5d80458e02be55e3ee67e9ef325cd2
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-09-02 10:01:26 +0300

    Fix routes

--- src/PublicRoute.jsx
+++ src/PublicRoute.jsx
@@ -1,15 +1,11 @@
 import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { useAuth } from "./shared/hooks/useAuth";
-import WelcomePage from "./pages/WelcomePage/WelcomePage";
 
 export const PublicRoute = ({ children, restricted }) => {
   const { isLoggedIn } = useAuth();
   const shouldRedirect = restricted && isLoggedIn;
-  if (!isLoggedIn) return <WelcomePage />;
-  if (shouldRedirect)
-    return !shouldRedirect ? children : <Navigate to="/home" />;
-  else return <PageNotFound />;
+  return !shouldRedirect ? children : <Navigate to="/home" />;
 };
 
 PublicRoute.propTypes = {
